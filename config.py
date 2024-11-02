dc57c0231857bee92106cdb28da1ce94a2e1472b
Fedor Tatarnikov <62100314+gosleeptoday@users.noreply.github.com>
2024-11-02 16:25:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 "Тут мы храним все настройки"
 
-TOKEN = "7487058319:AAEdA81Qqh2I2O9nrB1P1Ly3SFabVZFz6Yo"
-ADMIN_IDS = [773672154, 892327886]
+TOKEN = ""
+ADMIN_IDS = []
 db_url = "sqlite://database.db"
 modules = {"models": ["database.models"]}
 
